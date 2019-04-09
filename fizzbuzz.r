c198ce4282e0c1675b6561f9a381d60e39a72fa1
Haubergs <31439189+Haubergs@users.noreply.github.com>
2019-04-09 22:52:52 +0200

    Update fizzbuzz.r

--- fizzbuzz.r
+++ fizzbuzz.r
@@ -1,20 +1,23 @@
 #FizzBuzz in R
 
+#specific coding
 fizzBuzz=function(x){
- if(length(x)>1 | !is.integer(x)) stop("input must be a single integer")
- if( !(x%%3L) & !(x%%5L) ){
-   "FizzBuzz"
- }else if(!(x%%3L)){
-   "Fizz"
- }else if(!(x%%5L)){
-  "Buzz"
- }else{
-  x
- }
+  if(length(x)>1 | !is.integer(x)) stop("input must be a single integer")
+  if( !(x%%3L) & !(x%%5L) ){
+    "FizzBuzz"
+  }else if(!(x%%3L)){
+    "Fizz"
+  }else if(!(x%%5L)){
+    "Buzz"
+  }else{
+    x
+  }
 }
+ 
 sapply(1:100,fizzBuzz)
 
-
+ 
+#general coding
 wordGame=function(x,tags=c(Fizz=3L,Buzz=5L)){
   if(length(x)>1 | !is.integer(x)) stop("input must be a single integer")
   residuals= x %% tags #creates named vector with residuals of the tags
@@ -24,5 +27,5 @@ wordGame=function(x,tags=c(Fizz=3L,Buzz=5L)){
     paste(  names(residuals)[!residuals]  ,collapse="")
   }
 }
-
+ 
 sapply(1:100,wordGame)
